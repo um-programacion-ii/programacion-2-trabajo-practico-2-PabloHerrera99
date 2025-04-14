30166641246f100a8aeaf120ac31539d0019a4a9
PabloHerrera99 <p.herrera@alumno.um.edu.ar>
2025-04-14 20:31:59 -0300

    Se agrego la clase consola para control de menus

--- main.java
+++ main.java
@@ -1,23 +1,7 @@
-import modelos.Usuario;
-import gestores.GestorUsuarios;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class main {
     public static void main(String[] args) {
-
-        List<Usuario> listaUsuarios = new ArrayList<>();
-        GestorUsuarios gestorusuarios = new GestorUsuarios(listaUsuarios);
-
-
-        gestorusuarios.registrarUsuario();
-        gestorusuarios.registrarUsuario();
-       // gestorusuarios.registrarUsuario();
-
-        gestorusuarios.buscarUsuario();
-        gestorusuarios.eliminarUsuario();
-        gestorusuarios.buscarUsuario();
-
+        Consola consola = new Consola();
+        consola.menuPrincipal();
     }
 }
