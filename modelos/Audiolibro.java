2e692bc98ba886c5d6e86ad3058d8e7b4be4f583
PabloHerrera99 <p.herrera@alumno.um.edu.ar>
2025-04-15 11:33:29 -0300

    Se agrego a los menus de consola las opciones para gestionar los recursos (falta la opcion de poder listarlos)

--- modelos/Audiolibro.java
+++ modelos/Audiolibro.java
@@ -1,4 +1,39 @@
 package modelos;
 
-public class Audiolibro {
+public class Audiolibro extends RecursoDigital {
+    private String idioma;
+    private int duracionMinutos;
+
+    public Audiolibro(String titulo, String autor, String idioma, int duracionMinutos) {
+        super(titulo, autor);
+        this.idioma = idioma;
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setIdioma(String idioma) {
+        if (idioma == null || idioma.isEmpty()) {
+            throw new IllegalArgumentException("El audiolibro tiene que tener un idioma");
+        }
+        this.idioma = idioma;
+    }
+    public void setDuracionMinutos(int duracionMinutos) {
+        if (duracionMinutos < 0) {
+            throw new IllegalArgumentException("El audiolibro tiene que tener una duración mayor que 0");
+        }
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\n Idioma: " + idioma +
+                "\n Duracion minutos: " + duracionMinutos;
+    }
 }
--- modelos/Libro.java
+++ modelos/Libro.java
@@ -1,4 +1,36 @@
 package modelos;
 
-public class Libro {
+
+public class Libro extends RecursoDigital {
+    private String genero;
+    private String saga;
+
+    public Libro(String titulo, String autor, String genero, String saga) {
+        super(titulo, autor);
+        this.genero = genero;
+        this.saga = saga;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+    public String getSaga() {
+        return saga;
+    }
+
+    public void setGenero(String genero) {
+        if (genero == null || genero.isEmpty()) {
+            throw new IllegalArgumentException("El libro tiene que tener un genero");
+        }
+        this.genero = genero;
+    }
+    public void setSaga(String saga) {
+        this.saga = saga;
+    }
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nGenero: " + genero +
+                "\nSaga: " + saga;
+    }
 }
